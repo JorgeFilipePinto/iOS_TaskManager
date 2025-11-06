ed2acd32b46fc9f7351d89b093875102c66a19d7
Jorge Pinto <jorge.filipepinto@hotmail.com>
2025-11-06 21:23:11 +0000

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Jorge Filipe Correia Pinto on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
